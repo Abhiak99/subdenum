d287fdc53530a95f3c2442645d30a222b75967cc
Abhishek Kumar <78052345+Abhiak99@users.noreply.github.com>
2023-08-24 20:32:25 +0530

    Update csv2txt.py

--- csv2txt.py
+++ csv2txt.py
@@ -1,20 +1,32 @@
+#!/bin/bash
 import sys
-if len(sys.argv)!=2:
-	print("[*] Please provide the csv file ")
-	print("[*] Usage : python3 {} (csvfile.csv)".format(sys.argv[0]))
+if len(sys.argv)<2:
+    print("[*] Please provide the csv file ")
+    print("[*] Usage : python3 {} (csvfile.csv)".format(sys.argv[0]))
+    sys.exit()
+
+check = input("Bounty eligible : [Y] or [N]\n")
+if check=='Y':
+    bounty = 'true'
+else:
+    bounty = 'false'
 
 def change(line):
 	columns = line.split(',')
 	if "URL" in columns or "DOMAIN" in columns or "WILDCARD" in columns:
-		if(columns[3]=='true' and columns[4]=='true'):#Bounty | Inscope
+		if(columns[3]==bounty and columns[4]=='true'):#Bounty | Inscope
 		#if(columns[3]=='false' and columns[4]=='true'):#VDP | Inscope
 			return columns[0]
 			# print(columns[0],columns[1])
 
 
-with open("download_csv.csv", "rt") as csvfile, open("target_scope.txt", "wt") as target:
+with open(sys.argv[1], "rt") as csvfile, open("target_scope.txt", "wt") as target:
 	lines = csvfile.readlines()
 	for line in lines:
 		k = change(line)
 		if k!=None:
 			target.write(str(k).strip("http://").strip("*").strip(".")+"\n")
+			
+
+
+## This script is only for HackerOne Programs
