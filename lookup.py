8d2cf379cb78d9e9dff2357bad50b3e0262f53fe
Abhishek Kumar <78052345+Abhiak99@users.noreply.github.com>
2024-08-20 22:01:20 +0530

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -1,26 +1,33 @@
 from nslookup import Nslookup
 import json
 import sys
-if len(sys.argv) != 2:
-	print("[*] Usage python3 %s [subdomain.txt]" % sys.argv[0])
-	sys.exit()
-f = open(sys.argv[1], "rt")
-ips = {}
+try:
+	if len(sys.argv) != 2:
+		print("[*] Usage python3 %s [subdomain.txt]" % sys.argv[0])
+		sys.exit()
+	f = open(sys.argv[1], "rt")
+	ips = {}
 
-for domain in f:
-	dns_query = Nslookup(dns_servers=["8.8.8.8","1.1.1.1"], verbose=False, tcp=False)
+	for domain in f:
+		dns_query = Nslookup(dns_servers=["8.8.8.8","1.1.1.1"], verbose=False, tcp=False)
 
-	ips_record = dns_query.dns_lookup(domain.strip())
-	# print(ips_record.response_full, ips_record.answer)
+		ips_record = dns_query.dns_lookup(domain.strip())
+		# print(ips_record.response_full, ips_record.answer)
 
-	if ips_record.answer:
-            ips[domain.strip()]=ips_record.answer
-f.close()
+		if ips_record.answer:
+	            ips[domain.strip()]=ips_record.answer
+	f.close()
 
 
-f = open("alivesubdomains.txt","w")
-for i in ips.keys():
-    f.write(i+" "+str(ips[i])+"\n")
-f.close()
-y = json.dumps(ips)
-print(y)
+	f = open("alivesubdomains.txt","w")
+	for i in ips.keys():
+	    f.write(i+" "+str(ips[i])+"\n")
+	f.close()
+	y = json.dumps(ips)
+	print(y)
+
+except KeyboardInterrupt:
+    print("[!] Process interrupted by user (Ctrl+C). Exiting...")
+
+except Exception as e:
+	print("[!] Error: ",e)
